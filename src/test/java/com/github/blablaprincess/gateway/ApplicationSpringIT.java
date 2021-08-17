5be508a69959f8c866b4ff2447a41647c2e6d0bf
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-18 03:41:22 +1000

    Initial commit

---
+++ src/test/java/com/github/blablaprincess/gateway/ApplicationSpringIT.java
@@ -0,0 +1,13 @@
+package com.github.blablaprincess.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSpringIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
